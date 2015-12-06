04b415164e1350483b2c64d43843e2f2698f688c
Jingguo Yao <yaojingguo@gmail.com>
2015-12-06 20:01:22 +0800

    Update

--- tool.sh
+++ tool.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+  cat << EOF
+usage sample: $0 DirectedDFS data/tinyDG.txt 2
+EOF
+  exit 1
+fi
+
 classpath=build/classes/main
 
 java -cp $classpath $@
-# sample: ./tool.sh DirectedDFS algs4-data/tinyDG.txt 2
