b854c8d49f2ed7f18d14900e0604056988f5278e
Jingguo Yao <yaojingguo@gmail.com>
2016-02-04 10:58:21 +0800

    Update

---
+++ src/main/java/PercolationStats.java
@@ -0,0 +1,14 @@
+public class PercolationStats {
+   // perform T independent experiments on an N-by-N grid
+   public PercolationStats(int N, int T)     
+   // sample mean of percolation threshold
+   public double mean()                      
+   // sample standard deviation of percolation threshold
+   public double stddev()                    
+   // low  endpoint of 95% confidence interval
+   public double confidenceLo()              
+   // high endpoint of 95% confidence interval
+   public double confidenceHi()              
+   // test client (described below)
+   public static void main(String[] args)    
+}
