c412cf92d022fd810d4980c006f6888ff38e0b68
Jingguo Yao <yaojingguo@gmail.com>
2016-05-02 11:32:19 +0800

    Update

--- src/main/java/yao/Validate.java
+++ src/main/java/yao/Validate.java
@@ -6,6 +6,12 @@ public class Validate {
     public static void main(String[] args) {
         String regexp = args[0];
         String input = args[1];
-        StdOut.println(input.matches(regexp));
+        StdOut.println(validate(regexp, input));
+    }
+
+    public static boolean validate(String regexp, String input) {
+        StdOut.printf("regexp: %s\n", regexp);
+        StdOut.printf("input: %s\n", input);
+        return input.matches(regexp);
     }
 }
---
+++ src/test/java/yao/HarvesterTest.java
@@ -0,0 +1,12 @@
+package yao;
+
+import org.junit.Test;
+
+public class HarvesterTest {
+    
+    @Test
+    public void testHarvest() {
+        
+    }
+
+}
